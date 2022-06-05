fadaba4b1e988db6308928b33645cd5761127405
ferkruz <fercruzhigel@hotmail.com>
2022-06-04 23:53:54 -0300

    Desafío Componentes F.Cruz
    Componentes y Elementos de un proyecto Angular
    Tailwind
    Fernando Cruz

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { FooterComponent } from './footer/footer.component';
+import { FormComponent } from './form/form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+    FooterComponent,
+    FormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
